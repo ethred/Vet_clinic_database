a98a26b58a952837eaec8829ec84babb2bc8b90f
redwan <redtah2007@yahoo.com>
2023-09-26 03:43:42 -0700

    update schema and data insertion

--- schema.sql
+++ schema.sql
@@ -15,3 +15,35 @@ CREATE TABLE animals (
 ALTER TABLE animals
 ADD COLUMN species VARCHAR
 (255);
+
+CREATE TABLE owners
+(
+    id serial PRIMARY KEY,
+    full_name varchar(255),
+    age integer
+);
+
+/* Create a table named species and make sure that id is set as autoincremented into the PRIMARY KEY */
+CREATE TABLE species
+(
+    id serial PRIMARY KEY,
+    name varchar(255)
+);
+
+/* Modify the animals table */
+ALTER TABLE animals
+ALTER COLUMN id
+SET DATA TYPE
+serial;
+
+/* Remove species column inside the table */
+ALTER TABLE animals
+DROP COLUMN species;
+
+/* Add column species_id which is a foreign key referencing species table */
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
+
+/* Add column owner_id which is a foreign key referencing the owners table */
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id);
